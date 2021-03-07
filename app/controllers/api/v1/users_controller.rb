fd0d9d240628d1af253990273658d5b43e260827
Keli Booster <kbooster17@gmail.com>
2021-03-07 06:12:45 +0000

    setup heroku

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -24,6 +24,7 @@ module Api
       def create
         @user = User.new(user_params)
         if @user.save
+          setToken
           UserNotifierMailer.send_signup_email(@user).deliver
           render :show, status: :created
         else
@@ -36,8 +37,6 @@ module Api
       def update
         if current__user.isAdmin || current__user == @user
           if @user.update(user_params)
-           setToken
-            UserNotifierMailer.send_signup_email(@user).deliver
             render :show, status: :ok
           else
             render json: @user.errors, status: :unprocessable_entity
