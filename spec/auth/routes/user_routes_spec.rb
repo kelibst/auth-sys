8a2ff8c8dd00a741202577f1be59cd181f1f736e
Keli Booster <kbooster17@gmail.com>
2021-03-14 05:34:49 +0000

    add swagger helper

---
+++ spec/auth/routes/user_routes_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+require 'swagger_helper'
+
+describe 'Routes', type: :routing do
+
+  describe 'usersController' do
+    it 'routes get request to users index action' do
+      expect(get: '/api/v1/users/').to route_to(
+        controller: 'api/v1/users',
+        action: 'index'
+      )
+    end
+
+    it 'routes post request to users create action' do
+      expect(post: '/api/v1/users/').to route_to(
+        controller: 'api/v1/users',
+        action: 'create'
+      )
+    end
+
+    it 'routes get request with id to users show action' do
+      expect(get: '/api/v1/users/:id').to route_to(
+        controller: 'api/v1/users',
+        action: 'show',
+        id: ':id'
+      )
+    end
+
+    it 'routes put request with id to users update action' do
+      expect(put: '/api/v1/users/:id').to route_to(
+        controller: 'api/v1/users',
+        action: 'update',
+        id: ':id'
+      )
+    end
+
+    it 'routes delete request with id to users destroy action' do
+      expect(DELETE: '/api/v1/users/:id').to route_to(
+        controller: 'api/v1/users',
+        action: 'destroy',
+        id: ':id'
+      )
+    end
+  end
+end
