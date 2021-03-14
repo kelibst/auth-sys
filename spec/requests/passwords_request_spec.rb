41460c95cbe0cc7f76ffb9a9386b4c118f581eb0
Keli Booster <kbooster17@gmail.com>
2021-03-14 05:30:39 +0000

    add forgot and reset routes spec

--- spec/requests/passwords_request_spec.rb
+++ spec/requests/passwords_request_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
+require 'swagger_helper'
 
-RSpec.describe "Passwords", type: :request do
 
+RSpec.describe "Passwords", type: :routing do
+    it 'routes post forgot to forgot action' do
+        expect(post: 'api/v1/password/forgot').to route_to(
+          controller: 'api/v1/passwords',
+          action: 'forgot'
+        )
+    end
+
+      it 'routes post reset to reset action' do
+        expect(post: 'api/v1/password/reset').to route_to(
+          controller: 'api/v1/passwords',
+          action: 'reset'
+        )
+    end 
 end
