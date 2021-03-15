cfc62f3a1c2f020811af14797dab88a3dd1c02fd
Keli Booster <kbooster17@gmail.com>
2021-03-15 20:51:56 +0000

    turn api_only to true in application.rb

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module AuthSys
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = false
+    config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
